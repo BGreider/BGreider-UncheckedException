cf0cd39654d266f6a41fdd0784d43e1ff76225c9
BGreider <bgreider@gmail.com>
2023-02-28 23:40:52 +0000

    GreiderLab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -13,7 +13,11 @@ public class Lab {
      * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
      * event happens, like running out of computer memory.
      */
-    public void throwUncheckedException(){
+    int z = 20;
+    int divzero = 0;
 
+    public void throwUncheckedException(){
+ divzero = z / 0 ;
+ 
     }
 }
